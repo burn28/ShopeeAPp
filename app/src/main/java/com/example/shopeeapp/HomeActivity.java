eaf2ceff268f2e107b5461a28203f4f47f2005c1
Abu Bakar <abbakker00@gmail.com>
2021-05-29 18:32:53 +0800

    fix bottom navbar and add toolbar

--- app/src/main/java/com/example/shopeeapp/HomeActivity.java
+++ app/src/main/java/com/example/shopeeapp/HomeActivity.java
@@ -2,10 +2,14 @@ package com.example.shopeeapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.example.shopeeapp.fragment.DonateFragment;
@@ -21,6 +25,7 @@ public class HomeActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
     Fragment selectFragment = null;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,34 +33,47 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         bottomNavigationView = findViewById(R.id.bottom_nav_bar);
-        bottomNavigationView.setOnNavigationItemReselectedListener(navItemSelectListener);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navItemSelectListener);
+        toolbar = findViewById(R.id.app_toolbar);
+
+        setSupportActionBar(toolbar);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FeedFragment()).commit();
 
     }
 
-    private BottomNavigationView.OnNavigationItemReselectedListener navItemSelectListener =
-            new BottomNavigationView.OnNavigationItemReselectedListener() {
-        @Override
-        public void onNavigationItemReselected(@NonNull @NotNull MenuItem item) {
-            switch (item.getItemId()){
-                case R.id.nav_home:
-                    selectFragment = new FeedFragment();
-//                    bottomNavigationView.setLabelVisibilityMode(LabelVisibilityMode.LABEL_VISIBILITY_LABELED);
-                    break;
-                case R.id.nav_donate:
-                    selectFragment = new DonateFragment();
-                    break;
-                case R.id.nav_profile:
-                    SharedPreferences.Editor editor = getSharedPreferences("PREF", MODE_PRIVATE).edit();
-                    editor.putString("profileid", FirebaseAuth.getInstance().getCurrentUser().getUid());
-                    editor.apply();
-                    selectFragment = new ProfileFragment();
-                    break;
-            }
-            if(selectFragment!=null){
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectFragment).commit();
-            }
-        }
-    };
+    private BottomNavigationView.OnNavigationItemSelectedListener navItemSelectListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+                    switch(item.getItemId()){
+                        case R.id.nav_home:
+                            selectFragment = new FeedFragment();
+                            break;
+                        case R.id.nav_donate:
+                            selectFragment = new DonateFragment();
+                            break;
+                        case R.id.nav_profile:
+                            SharedPreferences.Editor editor = getSharedPreferences("PREF", MODE_PRIVATE).edit();
+                            editor.putString("profileid", FirebaseAuth.getInstance().getCurrentUser().getUid());
+                            editor.apply();
+                            selectFragment = new ProfileFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectFragment).commit();
+                    return true;
+                }
+            };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_toolbar, menu);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search_tool).getActionView();
+        searchView.setQueryHint("Search Item");
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
 }
